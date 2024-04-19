7b36b88563ac1843a92ae4cef1617088235bbe0e
Mark Angelo Nool <markangelonool0@gmail.com>
2024-04-19 19:02:36 +0800

    ups

--- Python read from file/readfile.py
+++ Python read from file/readfile.py
@@ -1,6 +1,6 @@
 import csv
 
-with open('cereal.csv', 'r') as csvfile:
-    reader = csv.DictReader(csvfile)
+with open('cereal.csv', 'r') as file:
+    reader = csv.DictReader(file)
     for row in reader:
         print(row)
