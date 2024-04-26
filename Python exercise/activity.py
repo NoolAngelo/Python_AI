7e88cadc678cc28a2ed9032f5eda41e52fc00652
Mark Angelo Nool <markangelonool0@gmail.com>
2024-04-26 14:58:15 +0800

    ups

--- Python exercise/activity.py
+++ Python exercise/activity.py
@@ -10,6 +10,13 @@ E_intersect_T = E.intersection(T)
 B_union_E_union_T = B.union(E, T)
 H_diff_E = H.difference(E)
 B_diff_E_union_T = B.difference(E).union(T)
+# Additional operations
+E_diff_B = E.difference(B)
+T_intersect_H = T.intersection(H)
+
+# Print the results
+print("E - B:", E_diff_B)
+print("T intersect H:", T_intersect_H)
 
 # Print the results
 print("B union H:", B_union_H)
@@ -17,3 +24,5 @@ print("E intersect T:", E_intersect_T)
 print("B union E union T:", B_union_E_union_T)
 print("H - E:", H_diff_E)
 print("B - E union T:", B_diff_E_union_T)
+print("E - B:", E_diff_B)
+print("T intersect H:", T_intersect_H)
