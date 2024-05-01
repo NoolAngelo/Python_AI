aff78f998882ed35e6d2f92969c35de22a788b20
Mark Angelo Nool <markangelonool0@gmail.com>
2024-05-01 22:21:26 +0800

    ups

--- Python exercise/Samples.py
+++ Python exercise/Samples.py
@@ -1,3 +1,5 @@
+import time
+
 name ="Nool/Angelo"
 
 first_name = name[:4]
@@ -13,3 +15,27 @@ print(funky_name)
 print(reversed_name)
 
 print(name[slice])
+
+
+for seconds in range(10,0,-1):
+    print(seconds)
+    time.sleep(1)
+print("bomba na!")
+
+food = ["pizza","burger","hotdog","pudding"]
+food[0] = "bomba na"
+
+food.append("ice cream")
+food.remove("burger")
+food.pop()
+food.insert(1,"cake")
+food.sort()
+
+for x in food:
+    print(x)
+
+drinks = ["coffee","soda","tea"]
+dinner = ["adobo","sinigang","tinola"]
+dessert = ["makapuno ice cream", "tubeg","mango"]
+food1 =[drinks,dinner,dessert]
+print(food1[2][2])
