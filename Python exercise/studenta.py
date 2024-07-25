d738ada6644ecc6ef04fbd20651765c4cb87a5b7
Mark Angelo Nool <markangelonool0@gmail.com>
2024-07-25 18:27:51 +0800

    deleted unecessary files

--- Python exercise/studenta.py
+++
@@ -1,6 +0,0 @@
-from student_class import student
-
-student1 = student("Jim",18 , "Business", 3.1, False)
-print(student1.name, student1.age, student1.major, student1.gpa, student1.is_on_probation)
-
-#update from pc
--- Python_random/working_with_string.py
+++
@@ -1,9 +0,0 @@
-txt ="My Name is Angelo and im {}"
-age=36
-x=txt.strip()
-print(txt.format(age))
-print(txt.format(x))
-
-print(bool("abc"))
-print(bool(0))   
-    
